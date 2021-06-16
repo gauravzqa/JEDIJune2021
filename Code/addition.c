72514b397ddd7130f8b7f2b84d2395fe557b7d26
gauravzqa <gauravzqa@gmail.com>
2021-06-16 15:01:49 +0530

    The following changes were made
    	addition.c
    		added the addition function
    	helloWorld.c
    		modified the helloWorld.c program

---
+++ Code/addition.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	sum(1,2);
+}
+
+int sum(int a, int b){
+	return a+b;
+}
--- Code/helloWorld.c
+++ Code/helloWorld.c
@@ -1,5 +1,12 @@
+//hello world program
+
 #include <stdio.h>
 
 int main(){
+	helloWorld();
+}
+
+void helloWorld(){
 	printf("Hello World!");
+	printf("lol xd!!!");
 }
