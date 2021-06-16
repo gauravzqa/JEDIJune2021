60561e7bc34c41623347f554caaadf8bbc9c1168
gauravzqa <gauravzqa@gmail.com>
2021-06-16 15:23:39 +0530

    Following Code Changes 	helloWorld.c 		Fixed Compliation Error 	Hello.java 		added the Hello.java file

---
+++ Code/Hello.java
@@ -0,0 +1,7 @@
+package learnjava;
+
+class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello World!!");
+	}
+}
--- Code/helloWorld.c
+++ Code/helloWorld.c
@@ -2,11 +2,13 @@
 
 #include <stdio.h>
 
-int main(){
-	helloWorld();
-}
-
 void helloWorld(){
 	printf("Hello World!");
 	printf("lol xd!!!");
 }
+
+int main(){
+	helloWorld();
+}
+
+
